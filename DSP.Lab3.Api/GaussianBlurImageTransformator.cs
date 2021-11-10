09f10e8456c21237ab41dd3dc0297e290f46c10a
ArtsemiLasyi <lasyartsemy01@gmail.com>
2021-11-10 10:31:44 +0300

    Gaussian blur completed

--- DSP.Lab3.Api/GaussianBlurImageTransformator.cs
+++ DSP.Lab3.Api/GaussianBlurImageTransformator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Linq;
 using System.Text;
 
 namespace DSP.Lab3.Api
@@ -11,41 +12,96 @@ namespace DSP.Lab3.Api
         public unsafe override Bitmap Transform(Bitmap bitmap, int windowSize)
         {
             Bitmap newBitmap = new Bitmap(bitmap);
+
             BitmapData bitmapData = newBitmap.LockBits(
                 new Rectangle(0, 0, newBitmap.Width, newBitmap.Height),
                 ImageLockMode.ReadWrite,
                 newBitmap.PixelFormat
             );
 
-            int pixelSize = Image.GetPixelFormatSize(newBitmap.PixelFormat) / 8; 
-            int width = newBitmap.Width * pixelSize;
-            int height = newBitmap.Height;
+            int pixelSize = Image.GetPixelFormatSize(newBitmap.PixelFormat) / 8;
 
-            double acR, acG, acB;
-            int radius = 300;
-            int size = 2 * radius + 6;
-
-            for (int i = 0; i < height; i++)
+            for (int i = 0; i < newBitmap.Height; i++)
             {
-                for (int j = 0; j < width; j = j + pixelSize)
+                for (int j = 0; j < newBitmap.Width * pixelSize; j += pixelSize)
                 {
                     byte* cursorPosition = (byte*)bitmapData.Scan0 + i * bitmapData.Stride;
 
-                    int R = cursorPosition[j + 1];
-                    int G = cursorPosition[j + 2];
-                    int B = cursorPosition[j + 3];
+                    int red = 0;
+                    int green = 0;
+                    int blue = 0;
 
-                    acR = R * (Math.Pow((double)Math.E, -(Math.Pow((double)j - size / 2, 2) + Math.Pow((double)i - size / 2, 2)) / (2 * Math.Pow((double)radius, 2))));
-                    acG = G * (Math.Pow((double)Math.E, -(Math.Pow((double)j - size / 2, 2) + Math.Pow((double)i - size / 2, 2)) / (2 * Math.Pow((double)radius, 2))));
-                    acB = B * (Math.Pow((double)Math.E, -(Math.Pow((double)j - size / 2, 2) + Math.Pow((double)i - size / 2, 2)) / (2 * Math.Pow((double)radius, 2))));
+                    int delta = windowSize / 2;
 
-                    cursorPosition[j + 1] = (byte)acR;
-                    cursorPosition[j + 2] = (byte)acG;
-                    cursorPosition[j + 3] = (byte)acB;
+                    int counter = 0;
+
+                    double[] coefs = GetCoefficients(windowSize);
+
+                    for (int k = 0; k < windowSize; k++)
+                    {
+                        int indexY = k + i - delta;
+                        if (indexY < 0 || indexY > newBitmap.Height - 1)
+                        {
+                            continue;
+                        }
+
+                        byte* otherCursorPosition = (byte*)bitmapData.Scan0 + indexY * bitmapData.Stride;
+                        for (int s = 0; s < windowSize * pixelSize; s += pixelSize)
+                        {
+                            int indexX = s + j - delta * pixelSize;
+                            if (indexX < 0 || indexX > (newBitmap.Width - 1) * pixelSize)
+                            {
+                                continue;
+                            }
+
+                            red += (int)(otherCursorPosition[indexX + 1] * coefs[counter]);
+                            green += (int)(otherCursorPosition[indexX + 2] * coefs[counter]);
+                            blue += (int)(otherCursorPosition[indexX + 3] * coefs[counter]);
+
+                            counter++;
+                        }
+                    }
+
+                    cursorPosition[j + 1] = (byte)(red);
+                    cursorPosition[j + 2] = (byte)(green);
+                    cursorPosition[j + 3] = (byte)(blue);
                 }
             }
             newBitmap.UnlockBits(bitmapData);
+
             return newBitmap;
+        }
+
+        private double[] GetCoefficients(int windowSize)
+        {
+            double sigma = 3.5;
+
+            double[] coefs = new double[windowSize * windowSize];
+
+            for (int i = 0; i < windowSize * windowSize; i++)
+            {
+                int row = i / windowSize;
+                int place = i % windowSize;
+
+                double xSquare = Math.Pow(row - (windowSize / 2 + 1), 2);
+                double ySquare = Math.Pow(place - (windowSize / 2 + 1), 2);
+                coefs[i] = 
+                    (1 / (2 * Math.PI * Math.Pow(sigma, 2))) 
+                    * Math.Pow(
+                        Math.E, 
+                        -(xSquare + ySquare) / (2 * Math.Pow(sigma, 2))
+                    );
+            }
+
+            double sum = coefs.Sum();
+
+            for (int i = 0; i < windowSize * windowSize; i++)
+            {
+                coefs[i] /= sum;
+            }
+
+
+            return coefs;
         }
     }
 }
--- DSP.Lab3.Api/SobelFilterImageTransformator.cs
+++ DSP.Lab3.Api/SobelFilterImageTransformator.cs
@@ -8,12 +8,9 @@ namespace DSP.Lab3.Api
 {
     public class SobelFilterImageTransformator : ImageTransformator
     {
-        public override Bitmap Transform(Bitmap bitmap, int windowSize)
+        public unsafe override Bitmap Transform(Bitmap bitmap, int windowSize)
         {
             Bitmap newBitmap = new Bitmap(bitmap);
-
-            int[,] kernelY = GetKernelY(windowSize);
-            int[,] kernelX = GetKernelX(windowSize);
 
             BitmapData bitmapData = newBitmap.LockBits(
                 new Rectangle(0, 0, newBitmap.Width, newBitmap.Height),
@@ -21,16 +18,75 @@ namespace DSP.Lab3.Api
                 newBitmap.PixelFormat
             );
 
-            int pixelSize = Image.GetPixelFormatSize(newBitmap.PixelFormat) / 8; // число бит на пиксель
+            int pixelSize = Image.GetPixelFormatSize(newBitmap.PixelFormat) / 8;
 
-            for (int i = 1; i <= newBitmap.Height - 2; i++)
+            for (int i = 0; i < newBitmap.Height; i++)
             {
-                for (int j = 1; j <= newBitmap.Width - 2; j++)
+                for (int j = 0; j < newBitmap.Width * pixelSize; j += pixelSize)
                 {
+                    byte* cursorPosition = (byte*)bitmapData.Scan0 + i * bitmapData.Stride;
 
+                    int redX = 0;
+                    int greenX = 0;
+                    int blueX = 0;
+                    int redY = 0;
+                    int greenY = 0;
+                    int blueY = 0;
+
+                    int delta = windowSize / 2;
+
+                    int counter = 0;
+
+                    int[,] kernelX = GetKernelX(windowSize);
+                    int[,] kernelY = GetKernelY(windowSize);
+
+                    for (int k = 0; k < windowSize; k++)
+                    {
+                        int indexY = k + i - delta;
+                        if (indexY < 0 || indexY > newBitmap.Height - 1)
+                        {
+                            continue;
+                        }
+
+                        byte* otherCursorPosition = (byte*)bitmapData.Scan0 + indexY * bitmapData.Stride;
+                        for (int s = 0; s < windowSize * pixelSize; s += pixelSize)
+                        {
+                            int indexX = s + j - delta * pixelSize;
+                            if (indexX < 0 || indexX > (newBitmap.Width - 1) * pixelSize)
+                            {
+                                continue;
+                            }
+
+                            redX += otherCursorPosition[indexX + 1] * kernelX[k, s / pixelSize];
+                            greenX += otherCursorPosition[indexX + 2] * kernelX[k, s / pixelSize];
+                            blueX += otherCursorPosition[indexX + 3] * kernelX[k, s / pixelSize];
+                            redY += otherCursorPosition[indexX + 1] * kernelY[k, s / pixelSize];
+                            greenY += otherCursorPosition[indexX + 2] * kernelY[k, s / pixelSize];
+                            redY += otherCursorPosition[indexX + 3] * kernelY[k, s / pixelSize];
+
+                            counter++;
+                        }
+                    }
+
+                    int red = (byte)Math.Sqrt(
+                        Math.Pow(redX, 2)
+                        + Math.Pow(redY, 2)
+                    );
+                    int green = (byte)Math.Sqrt(
+                        Math.Pow(greenX, 2)
+                        + Math.Pow(greenY, 2)
+                    );
+                    int blue = (byte)Math.Sqrt(
+                        Math.Pow(blueX, 2)
+                        + Math.Pow(blueY, 2)
+                    );
+
+                    cursorPosition[j] = 255;
+                    cursorPosition[j + 1] = (byte)(red / Math.Pow(windowSize, 2));
+                    cursorPosition[j + 2] = (byte)(green / Math.Pow(windowSize, 2));
+                    cursorPosition[j + 3] = (byte)(blue / Math.Pow(windowSize, 2));
                 }
             }
-
             newBitmap.UnlockBits(bitmapData);
 
             return newBitmap;
